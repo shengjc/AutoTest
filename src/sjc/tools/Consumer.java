4a01a572d588225e8991ae3098efbc82d89f75e4
shengjc <sjcsnake@hotmail.com>
2017-05-11 18:00:47 +0800

    MQ测试工具

---
+++ src/sjc/tools/Consumer.java
@@ -0,0 +1,67 @@
+package sjc.tools;
+
+import java.io.IOException;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Consumer {
+    public static final String url = "tcp://172.16.0.17:61616";// 缺省端口，如果要改，可在apache-activemq\conf中的activemq.xml中更改端口号
+    ConnectionFactory factory;
+    Connection connection;
+    Session session;
+    MessageConsumer[] consumers;
+    ComunicateMode comunicateMode = ComunicateMode.pubsub;
+
+    enum ComunicateMode {
+        p2p, pubsub
+    }
+
+    public Consumer(ComunicateMode mode, String[] destinationNames)
+            throws JMSException {
+        this.comunicateMode = mode;
+        factory = new ActiveMQConnectionFactory(url);// 这里的url也可以不指定，java代码将默认将端口赋值为61616
+        connection = factory.createConnection();
+        connection.start();
+        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        consumers = new MessageConsumer[destinationNames.length];
+        for (int i = 0; i < destinationNames.length; i++) {
+            Destination destination = comunicateMode == ComunicateMode.pubsub ? session
+                    .createTopic( destinationNames[i]) : session
+                    .createQueue( destinationNames[i]);                    
+            consumers[i] = session.createConsumer(destination);
+            consumers[i].setMessageListener(new MessageListener() {
+                @Override
+                public void onMessage(Message message) {
+                    try {
+                        System.out.println(String.format("收到消息【%s】",
+                                ((TextMessage) message).getText()));
+                    } catch (JMSException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
+    public void close() throws JMSException {
+        if (connection != null)
+            connection.close();
+    }
+
+    public static void main(String[] args) throws JMSException, IOException {
+        Consumer consumer = new Consumer(ComunicateMode.pubsub,
+                new String[] {"winsion.kingkong.monitor.topic"});// 这里可以修改消息传输方式为pubsub
+        System.in.read();
+        consumer.close();
+    }
+
+}
---
+++ src/sjc/tools/Publisher.java
@@ -0,0 +1,89 @@
+package sjc.tools;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.StringTokenizer;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Publisher {
+    public static final String url = "tcp://localhost:61616";// 缺省端口，如果要改，可在apache-activemq-5.13.3\conf中的activemq.xml中更改端口号
+    ConnectionFactory factory;	// 连接工厂
+    Connection connection;	// 连接
+    Session session;	// 会话 接受或者发送消息的线程
+    MessageProducer producer;	// 消息生产者
+    Destination[] destinations;	// 消息的目的地
+    ComunicateMode comunicateMode = ComunicateMode.pubsub;
+
+    enum ComunicateMode {
+        p2p, pubsub
+    }
+
+    public Publisher(ComunicateMode mode) throws JMSException {
+        this.comunicateMode = mode;
+     // 实例化连接工厂
+        factory = new ActiveMQConnectionFactory(url);// 这里的url也可以不指定，java代码将默认将端口赋值为61616
+        connection = factory.createConnection();	// 通过连接工厂获取连接
+        try {
+            connection.start();	// 启动连接
+        } catch (JMSException e) {
+            connection.close();	//异常情况关闭连接
+            throw e;
+        }
+        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);	// 创建Session,第一个参数是开启事务true,第二个参数是消息确认机制属性
+        producer = session.createProducer(null); //创建生产者
+    }
+
+    //设置目的地，支持Topic和Queue两种方式
+    protected void setDestinations(String[] stocks) throws JMSException {
+        destinations = new Destination[stocks.length];
+        for (int i = 0; i < stocks.length; i++) {
+            destinations[i] = comunicateMode == ComunicateMode.pubsub ? session
+                    .createTopic("Topic." + stocks[i]) : session
+                    .createQueue("Queue." + stocks[i]);
+        }
+    }
+
+    //发送消息
+    protected void sendMessage(String msg) throws JMSException {
+        for (Destination item : destinations) {
+            TextMessage msgMessage = session.createTextMessage(msg);
+            producer.send(item, msgMessage);
+            System.out.println(String.format("成功向Topic為【%s】发送消息【%s】",
+                    item.toString(), msgMessage.getText()));
+        }
+    }
+
+    protected void close() throws JMSException {
+        if (connection != null)
+            connection.close();
+    }
+
+    public static void main(String[] args) throws JMSException,
+            InterruptedException, IOException {
+        Publisher publisher = new Publisher(ComunicateMode.pubsub);// 这里可以修改消息传输方式为pubsub
+        publisher.setDestinations(new String[] { "1", "2", "3" });
+        BufferedReader reader = null;
+        String contentString = "";
+        do {
+            System.out.println("请输入要发送的内容(exit退出):");
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            contentString = reader.readLine();
+            if (contentString.equals("exit"))
+                break;
+            publisher.sendMessage(contentString);
+        } while (!contentString.equals("exit"));
+        reader.close();
+        publisher.close();
+    }
+}
